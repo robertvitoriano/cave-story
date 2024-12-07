5ad52f9b7b89b4cc5b732f7fc20dcadca9858baf
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-12-07 16:18:13 -0300

    rename maps

--- source/src/game.cpp
+++ source/src/game.cpp
@@ -8,76 +8,94 @@
  * This class holds all information for our main game loop
  */
 
-namespace {
+namespace
+{
 	const int FPS = 50;
 	const int MAX_FRAME_TIME = 1000 / FPS;
 }
 
-Game::Game() {
+Game::Game()
+{
 	SDL_Init(SDL_INIT_EVERYTHING);
 	this->gameLoop();
 }
 
-Game::~Game() {
-
+Game::~Game()
+{
 }
 
-void Game::gameLoop() {
+void Game::gameLoop()
+{
 	Graphics graphics;
 	Input input;
 	SDL_Event event;
 
-	this->_level = Level("Map 1", graphics);
+	this->_level = Level("Map_1", graphics);
 	this->_player = Player(graphics, this->_level.getPlayerSpawnPoint());
 	this->_hud = HUD(graphics, this->_player);
 
 	int LAST_UPDATE_TIME = SDL_GetTicks();
-	//Start the game loop
-	while (true) {
+	// Start the game loop
+	while (true)
+	{
 		input.beginNewFrame();
 
-		if (SDL_PollEvent(&event)) {
-			if (event.type == SDL_KEYDOWN) {
-				if (event.key.repeat == 0) {
+		if (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_KEYDOWN)
+			{
+				if (event.key.repeat == 0)
+				{
 					input.keyDownEvent(event);
 				}
 			}
-			else if (event.type == SDL_KEYUP) {
+			else if (event.type == SDL_KEYUP)
+			{
 				input.keyUpEvent(event);
 			}
-			else if (event.type == SDL_QUIT) {
+			else if (event.type == SDL_QUIT)
+			{
 				return;
 			}
 		}
-		if (input.wasKeyPressed(SDL_SCANCODE_ESCAPE) == true) {
+		if (input.wasKeyPressed(SDL_SCANCODE_ESCAPE) == true)
+		{
 			return;
 		}
-		else if (input.isKeyHeld(SDL_SCANCODE_LEFT) == true) {
+		else if (input.isKeyHeld(SDL_SCANCODE_LEFT) == true)
+		{
 			this->_player.moveLeft();
 		}
-		else if (input.isKeyHeld(SDL_SCANCODE_RIGHT) == true) {
+		else if (input.isKeyHeld(SDL_SCANCODE_RIGHT) == true)
+		{
 			this->_player.moveRight();
 		}
 
-		if (input.isKeyHeld(SDL_SCANCODE_UP) == true) {
+		if (input.isKeyHeld(SDL_SCANCODE_UP) == true)
+		{
 			this->_player.lookUp();
 		}
-		else if (input.isKeyHeld(SDL_SCANCODE_DOWN) == true) {
+		else if (input.isKeyHeld(SDL_SCANCODE_DOWN) == true)
+		{
 			this->_player.lookDown();
 		}
 
-		if (input.wasKeyReleased(SDL_SCANCODE_UP) == true) {
+		if (input.wasKeyReleased(SDL_SCANCODE_UP) == true)
+		{
 			this->_player.stopLookingUp();
 		}
-		if (input.wasKeyReleased(SDL_SCANCODE_DOWN) == true) {
+		if (input.wasKeyReleased(SDL_SCANCODE_DOWN) == true)
+		{
 			this->_player.stopLookingDown();
 		}
 
-		if (input.wasKeyPressed(SDL_SCANCODE_Z) == true) {
+		if (input.wasKeyPressed(SDL_SCANCODE_Z) == true)
+		{
 			this->_player.jump();
 		}
 
-		if (!input.isKeyHeld(SDL_SCANCODE_LEFT) && !input.isKeyHeld(SDL_SCANCODE_RIGHT)) {
+		if (!input.isKeyHeld(SDL_SCANCODE_LEFT) && !input.isKeyHeld(SDL_SCANCODE_RIGHT))
+		{
 			this->_player.stopMoving();
 		}
 
@@ -92,7 +110,8 @@ void Game::gameLoop() {
 	}
 }
 
-void Game::draw(Graphics &graphics) {
+void Game::draw(Graphics &graphics)
+{
 	graphics.clear();
 
 	this->_level.draw(graphics);
@@ -103,30 +122,35 @@ void Game::draw(Graphics &graphics) {
 	graphics.flip();
 }
 
-void Game::update(float elapsedTime) {
+void Game::update(float elapsedTime)
+{
 	this->_player.update(elapsedTime);
 	this->_level.update(elapsedTime, this->_player);
 	this->_hud.update(elapsedTime, this->_player);
 
-	//Check collisions
+	// Check collisions
 	std::vector<Rectangle> others;
-	if ((others = this->_level.checkTileCollisions(this->_player.getBoundingBox())).size() > 0) {
-		//Player collided with at least one tile. Handle it.
+	if ((others = this->_level.checkTileCollisions(this->_player.getBoundingBox())).size() > 0)
+	{
+		// Player collided with at least one tile. Handle it.
 		this->_player.handleTileCollisions(others);
 	}
-	//Check slopes
+	// Check slopes
 	std::vector<Slope> otherSlopes;
-	if ((otherSlopes = this->_level.checkSlopeCollisions(this->_player.getBoundingBox())).size() > 0) {
+	if ((otherSlopes = this->_level.checkSlopeCollisions(this->_player.getBoundingBox())).size() > 0)
+	{
 		this->_player.handleSlopeCollisions(otherSlopes);
 	}
-	//Check doors
+	// Check doors
 	std::vector<Door> otherDoors;
-	if ((otherDoors = this->_level.checkDoorCollisions(this->_player.getBoundingBox())).size() > 0) {
+	if ((otherDoors = this->_level.checkDoorCollisions(this->_player.getBoundingBox())).size() > 0)
+	{
 		this->_player.handleDoorCollision(otherDoors, this->_level, this->_graphics);
 	}
-	//Check enemies
-	std::vector<Enemy*> otherEnemies;
-	if ((otherEnemies = this->_level.checkEnemyCollisions(this->_player.getBoundingBox())).size() > 0) {
+	// Check enemies
+	std::vector<Enemy *> otherEnemies;
+	if ((otherEnemies = this->_level.checkEnemyCollisions(this->_player.getBoundingBox())).size() > 0)
+	{
 		this->_player.handleEnemyCollisions(otherEnemies);
 	}
 }
