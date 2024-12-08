03dd931d8ed8a4047dddcd0b6e0204250629296e
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-12-08 18:47:45 -0300

    remove enemy class changes

--- source/headers/enemy.h
+++ source/headers/enemy.h
@@ -29,4 +29,21 @@ protected:
 	int _currentHealth;
 };
 
+class Bat : public Enemy
+{
+public:
+	Bat();
+	Bat(Graphics &graphics, Vector2 spawnPoint);
+	void update(int elapsedTime, Player &player);
+	void draw(Graphics &graphics);
+	void touchPlayer(Player *player);
+
+	void animationDone(std::string currentAnimation);
+	void setupAnimations();
+
+private:
+	float _startingX, _startingY;
+	bool _shouldMoveUp;
+};
+
 #endif
