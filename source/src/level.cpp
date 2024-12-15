de8351d6a74b941c1bb3aaa7c77f784464b112f1
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-12-15 13:00:43 -0300

    parse collision

--- source/src/level.cpp
+++ source/src/level.cpp
@@ -75,77 +75,269 @@ void Level::loadMap(std::string mapName, Graphics &graphics)
 	}
 	for (nlohmann::json layer : levelData["layers"])
 	{
-		int tileCounter = 0;
-		for (nlohmann::json tileGID : layer["data"])
+		if (layer["type"] == "tilelayer")
 		{
-			if (tileGID == 0)
-				continue;
 
-			// Get the tileset for this specific gid
-
-			Tileset tls;
-			int closest = 0;
-			for (int i = 0; i < this->_tilesets.size(); i++)
+			int tileCounter = 0;
+			for (nlohmann::json tileGID : layer["data"])
 			{
-				if (this->_tilesets[i].FirstGid <= tileGID)
+				if (tileGID == 0)
+					continue;
+
+				// Get the tileset for this specific gid
+
+				Tileset tls;
+				int closest = 0;
+				for (int i = 0; i < this->_tilesets.size(); i++)
 				{
-					if (this->_tilesets[i].FirstGid > closest)
+					if (this->_tilesets[i].FirstGid <= tileGID)
 					{
-						closest = this->_tilesets[i].FirstGid;
-						tls = this->_tilesets.at(i);
+						if (this->_tilesets[i].FirstGid > closest)
+						{
+							closest = this->_tilesets[i].FirstGid;
+							tls = this->_tilesets.at(i);
+						}
 					}
 				}
-			}
 
-			if (tls.FirstGid == -1)
-			{
-				// No tileset was found for this gid
+				if (tls.FirstGid == -1)
+				{
+					// No tileset was found for this gid
+					tileCounter++;
+				}
+
+				// Get the position of the tile in the level
+				int xx = 0;
+				int yy = 0;
+				xx = tileCounter % width;
+				xx *= tileWidth;
+				yy += tileHeight * (tileCounter / width);
+				Vector2 finalTilePosition = Vector2(xx, yy);
+
+				// Calculate the position of the tile in the tileset
+				Vector2 finalTilesetPosition = this->getTilesetPosition(tls, tileGID, tileWidth, tileHeight);
+
+				// Build the actual tile and add it to the level's tile list
+				bool isAnimatedTile = false;
+				AnimatedTileInfo ati;
+				for (int i = 0; i < this->_animatedTileInfos.size(); i++)
+				{
+					if (this->_animatedTileInfos.at(i).StartTileId == tileGID)
+					{
+						ati = this->_animatedTileInfos.at(i);
+						isAnimatedTile = true;
+						break;
+					}
+				}
+				if (isAnimatedTile == true)
+				{
+					std::vector<Vector2> tilesetPositions;
+					for (int i = 0; i < ati.TileIds.size(); i++)
+					{
+						tilesetPositions.push_back(this->getTilesetPosition(tls, ati.TileIds.at(i),
+																																tileWidth, tileHeight));
+					}
+					AnimatedTile tile(tilesetPositions, ati.Duration,
+														tls.Texture, Vector2(tileWidth, tileHeight), finalTilePosition);
+					this->_animatedTileList.push_back(tile);
+				}
+				else
+				{
+					Tile tile(tls.Texture, Vector2(tileWidth, tileHeight),
+										finalTilesetPosition, finalTilePosition);
+					this->_tileList.push_back(tile);
+				}
 				tileCounter++;
 			}
-
-			// Get the position of the tile in the level
-			int xx = 0;
-			int yy = 0;
-			xx = tileCounter % width;
-			xx *= tileWidth;
-			yy += tileHeight * (tileCounter / width);
-			Vector2 finalTilePosition = Vector2(xx, yy);
-
-			// Calculate the position of the tile in the tileset
-			Vector2 finalTilesetPosition = this->getTilesetPosition(tls, tileGID, tileWidth, tileHeight);
-
-			// Build the actual tile and add it to the level's tile list
-			bool isAnimatedTile = false;
-			AnimatedTileInfo ati;
-			for (int i = 0; i < this->_animatedTileInfos.size(); i++)
-			{
-				if (this->_animatedTileInfos.at(i).StartTileId == tileGID)
-				{
-					ati = this->_animatedTileInfos.at(i);
-					isAnimatedTile = true;
-					break;
-				}
-			}
-			if (isAnimatedTile == true)
-			{
-				std::vector<Vector2> tilesetPositions;
-				for (int i = 0; i < ati.TileIds.size(); i++)
-				{
-					tilesetPositions.push_back(this->getTilesetPosition(tls, ati.TileIds.at(i),
-																															tileWidth, tileHeight));
-				}
-				AnimatedTile tile(tilesetPositions, ati.Duration,
-													tls.Texture, Vector2(tileWidth, tileHeight), finalTilePosition);
-				this->_animatedTileList.push_back(tile);
-			}
-			else
-			{
-				Tile tile(tls.Texture, Vector2(tileWidth, tileHeight),
-									finalTilesetPosition, finalTilePosition);
-				this->_tileList.push_back(tile);
-			}
-			tileCounter++;
 		}
+		if (layer["type"] == "objectgroup")
+		{
+			if (layer["name"] == "collisions")
+			{
+				for (nlohmann::json object : layer["objects"])
+				{
+					float x, y, width, height;
+					x = object["x"];
+					y = object["y"];
+					width = object["width"];
+					height = object["height"];
+					this->_collisionRects.push_back(Rectangle(
+							std::ceil(x) * globals::SPRITE_SCALE,
+							std::ceil(y) * globals::SPRITE_SCALE,
+							std::ceil(width) * globals::SPRITE_SCALE,
+							std::ceil(height) * globals::SPRITE_SCALE));
+				}
+			}
+		}
+		// else if (objectLayerName == "slopes")
+		// {
+		// 	XMLElement *pObject = pObjectGroup->FirstChildElement("object");
+
+		// 	while (pObject)
+		// 	{
+		// 		std::vector<Vector2> points;
+		// 		Vector2 p1;
+		// 		p1 = Vector2(std::ceil(pObject->FloatAttribute("x")), std::ceil(pObject->FloatAttribute("y")));
+
+		// 		XMLElement *pPolyline = pObject->FirstChildElement("polyline");
+		// 		if (pPolyline != NULL)
+		// 		{
+		// 			std::vector<std::string> pairs;
+		// 			const char *pPoints = pPolyline->Attribute("points");
+
+		// 			std::string pointsParsed = this->parseString(pPoints);
+		// 			Utils::split(pointsParsed, pairs, ' ');
+		// 			// Now we have each of the pairs. Loop through the list of pairs
+		// 			// and split them into Vector2s and then store them in our points vector
+		// 			for (int i = 0; i < pairs.size(); i++)
+		// 			{
+		// 				std::vector<std::string> ps;
+		// 				Utils::split(pairs.at(i), ps, ',');
+		// 				points.push_back(Vector2(std::stoi(ps.at(0)), std::stoi(ps.at(1))));
+		// 			}
+		// 		}
+
+		// 		for (int i = 0; i < points.size(); i += 2)
+		// 		{
+		// 			this->_slopes.push_back(Slope(
+		// 					Vector2((p1.x + points.at(i < 2 ? i : i - 1).x) * globals::SPRITE_SCALE,
+		// 									(p1.y + points.at(i < 2 ? i : i - 1).y) * globals::SPRITE_SCALE),
+		// 					Vector2((p1.x + points.at(i < 2 ? i + 1 : i).x) * globals::SPRITE_SCALE,
+		// 									(p1.y + points.at(i < 2 ? i + 1 : i).y) * globals::SPRITE_SCALE)));
+		// 		}
+
+		// 		pObject = pObject->NextSiblingElement("object");
+		// 	}
+		// }
+		// else if (objectLayerName == "spawn points")
+		// {
+		// 	XMLElement *pObject = pObjectGroup->FirstChildElement("object");
+		// 	while (pObject)
+		// 	{
+		// 		float x = pObject->FloatAttribute("x");
+		// 		float y = pObject->FloatAttribute("y");
+		// 		const char *name = pObject->Attribute("name");
+		// 		std::string parsedString = this->parseString(name);
+		// 		if (parsedString == "player")
+		// 		{
+		// 			this->_spawnPoint = Vector2(std::ceil(x) * globals::SPRITE_SCALE,
+		// 																	std::ceil(y) * globals::SPRITE_SCALE);
+		// 		}
+
+		// 		pObject = pObject->NextSiblingElement("object");
+		// 	}
+		// }
+		// else if (objectLayerName == "doors")
+		// {
+		// 	XMLElement *pObject = pObjectGroup->FirstChildElement("object");
+
+		// 	while (pObject)
+		// 	{
+		// 		float x = pObject->FloatAttribute("x");
+		// 		float y = pObject->FloatAttribute("y");
+		// 		float w = pObject->FloatAttribute("width");
+		// 		float h = pObject->FloatAttribute("height");
+		// 		Rectangle rect = Rectangle(x, y, w, h);
+		// 		Vector2 spawnPosition = {0, 0};
+		// 		std::string destination;
+		// 		XMLElement *pProperties = pObject->FirstChildElement("properties");
+
+		// 		while (pProperties)
+		// 		{
+		// 			XMLElement *pProperty = pProperties->FirstChildElement("property");
+
+		// 			while (pProperty)
+		// 			{
+		// 				const char *name = pProperty->Attribute("name");
+		// 				std::string propertyName = this->parseString(name);
+		// 				if (propertyName == "destination")
+		// 				{
+		// 					const char *value = pProperty->Attribute("value");
+		// 					destination = this->parseString(value);
+		// 				}
+		// 				if (propertyName == "spawn_position")
+		// 				{
+		// 					const char *value = pProperty->Attribute("value");
+		// 					spawnPosition = this->parsePosition(value);
+		// 				}
+		// 				pProperty = pProperty->NextSiblingElement("property");
+		// 			}
+		// 			pProperties = pProperties->NextSiblingElement("properties");
+		// 		}
+
+		// 		Door door = Door(rect, destination, spawnPosition);
+		// 		this->_doorList.push_back(door);
+		// 		pObject = pObject->NextSiblingElement("object");
+		// 	}
+		// }
+		// else if (objectLayerName == "level_passages")
+		// {
+		// 	XMLElement *pObject = pObjectGroup->FirstChildElement("object");
+
+		// 	while (pObject)
+		// 	{
+		// 		std::string destination;
+		// 		Vector2 spawnPosition = {0, 0};
+
+		// 		float x = pObject->FloatAttribute("x");
+		// 		float y = pObject->FloatAttribute("y");
+		// 		float w = pObject->FloatAttribute("width");
+		// 		float h = pObject->FloatAttribute("height");
+		// 		Rectangle rect = Rectangle(x, y, w, h);
+
+		// 		XMLElement *pProperties = pObject->FirstChildElement("properties");
+
+		// 		while (pProperties)
+		// 		{
+		// 			XMLElement *pProperty = pProperties->FirstChildElement("property");
+
+		// 			while (pProperty)
+		// 			{
+		// 				const char *name = pProperty->Attribute("name");
+		// 				std::string propertyName = this->parseString(name);
+		// 				if (propertyName == "destination")
+		// 				{
+		// 					const char *value = pProperty->Attribute("value");
+		// 					destination = this->parseString(value);
+		// 				}
+		// 				if (propertyName == "spawn_position")
+		// 				{
+		// 					const char *value = pProperty->Attribute("value");
+		// 					spawnPosition = this->parsePosition(value);
+		// 				}
+		// 				pProperty = pProperty->NextSiblingElement("property");
+		// 			}
+
+		// 			pProperties = pProperties->NextSiblingElement("properties");
+		// 		}
+
+		// 		LevelPassage levelPassage = LevelPassage(rect, destination, spawnPosition);
+		// 		this->_levelPassagesList.push_back(levelPassage);
+		// 		pObject = pObject->NextSiblingElement("object");
+		// 	}
+		// }
+		// else if (objectLayerName == "enemies")
+		// {
+		// 	float x, y;
+		// 	XMLElement *pObject = pObjectGroup->FirstChildElement("object");
+
+		// 	while (pObject)
+		// 	{
+		// 		x = pObject->FloatAttribute("x");
+		// 		y = pObject->FloatAttribute("y");
+		// 		const char *name = pObject->Attribute("name");
+		// 		std::string enemyName = this->parseString(name);
+		// 		if (enemyName == "bat")
+		// 		{
+		// 			this->_enemies.push_back(new Bat(graphics, Vector2(std::floor(x) * globals::SPRITE_SCALE,
+		// 																												 std::floor(y) * globals::SPRITE_SCALE)));
+		// 		}
+
+		// 		pObject = pObject->NextSiblingElement("object");
+		// 	}
+		// }
+
+		// pObjectGroup = pObjectGroup->NextSiblingElement("objectgroup");
 	}
 }
 
